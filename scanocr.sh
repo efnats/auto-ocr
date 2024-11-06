47361dda3ec319eb76240199e65136bdfa35b892
efnats <efnats@users.noreply.github.com>
2024-11-06 02:09:18 +0100

    Update to accept command-line arguments for watch_dir and output_dir

--- scanocr.sh
+++ scanocr.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
-# Uncomment the next line if you need to install ocrmypdf and inotify-tools
-# install ocrmypdf tesseract-ocr-deu inotify-tools
+# Check for required arguments
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <watch_dir> <output_dir>"
+    exit 1
+fi
 
-# Display current date and time
-echo
-echo "Current date and time: $(date)"
-echo
-# List of directories to monitor
-directories=(
-"/path/to/dir1"
-"/path/to/dir2"
-"/path/to/dir3"
-# Add more directories if needed
-)
+# Directories passed as arguments
+watch_dir=$1
+output_dir=$2
 
 # Function to OCR incoming files
 ocr_new_file() {
     incoming_dir=$1
     incoming_file=$2
-    processed_dir="${incoming_dir}/.."
-
-
-    # Generate a timestamped filename
     timestamp=$(date +"%Y%m%d-%H%M%S")
     timestamped_file="${timestamp}.pdf"
 
@@ -31,12 +22,11 @@ ocr_new_file() {
     # Rename incoming file with timestamp
     mv "${incoming_dir}/${incoming_file}" "${incoming_dir}/${timestamped_file}"
 
-    # Perform OCR on the file and move it to the processed directory
-    ocrmypdf -d -c -i -r -l eng+deu -q "${incoming_dir}/${timestamped_file}" "${processed_dir}/${timestamped_file}"
+    # Perform OCR on the file and move it to the output directory
+    ocrmypdf -d -c -i -r -l eng+deu -q "${incoming_dir}/${timestamped_file}" "${output_dir}/${timestamped_file}"
 
-    if [ $? -eq 0 ]
-    then
-        echo "File processed and moved to ${processed_dir}/${timestamped_file}"
+    if [ $? -eq 0 ]; then
+        echo "File processed and moved to ${output_dir}/${timestamped_file}"
         
         # Remove the original file from the incoming directory
         rm -f "${incoming_dir}/${timestamped_file}"
@@ -45,16 +35,9 @@ ocr_new_file() {
     fi
 }
 
-# Monitor each provided directory for new files and process them
-for dir in "${directories[@]}"
-do
-    echo "Watching directories: ${dir}"
-    inotifywait -m -e close_write --format "%f" "${dir}" \
-    | while read -r incoming_file
-    do
-        ocr_new_file "${dir}" "${incoming_file}"
-    done &
+# Monitor the specified directory for new files and process them
+echo "Watching directory: ${watch_dir}"
+inotifywait -m -e close_write --format "%f" "${watch_dir}" | while read -r incoming_file; do
+    sleep 1
+    ocr_new_file "${watch_dir}" "${incoming_file}"
 done
-
-# Wait for all background jobs to complete
-wait
